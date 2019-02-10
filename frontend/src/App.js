d44170662ebab412783cd5cd9f367333e67039e3
Matthew Retoff <matt.retoff@yahoo.com>
2019-02-10 13:03:39 -0500

    Added input for ingredients; Made IngredientsList file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.png';
 
+import IngredientsList from "./IngredientsList";
+
 class App extends Component {
   render() {
     return (
-      <body id="background">
+      <div id="layout">
         <div id="top-border">
           <img src={logo} alt="" id="logo"/>
           <h2 id="title">Fridge2Food</h2>
         </div>
-      </body>
+        <IngredientsList />
+      </div>
     );
   }
 }
