fc2b5c2b50c76922a283670e04e89e91286126ed
Anthony Kiselev <akiselev@purdue.edu>
2019-02-13 18:11:40 -0500

    Added basic models file

---
+++ backend/app/models.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    token = db.Column(db.String(32), index=True, unique=True)
+    def emptyFunction():
+        pass
+
+class Recipe(db.Model):
+    def emptyFunction():
+        pass
+
