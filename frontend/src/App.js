7ef0e0a1d888e8747ac80d578985d4bc0a35dc0d
Vivian Guo <vguo@purdue.edu>
2019-02-14 21:26:27 -0500

    install react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.png';
 
+import RecipeList from "./components/RecipeList";
 import IngredientsList from "./components/IngredientsList";
 import NestedLogin from './components/nested.js'
 import SearchBar from './components/SearchBar.js'
+import IngredientsChecklist from './components/IngredientsChecklist';
+import RecipeSearch from './components/RecipeSearch';
 
 class App extends Component {
   render() {
@@ -19,6 +22,9 @@ class App extends Component {
         </div>
         <IngredientsList />
         <SearchBar />
+        <RecipeSearch />
+        <IngredientsChecklist />
+        <RecipeList />
       </div>
     );
   }
