8af5e2226a601b1d9c9dc4aedee5e5868bdc42c2
adsank99 <sankara2@purdue.edu>
2019-02-17 11:54:05 -0500

    added links for all recipies in list

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -1,15 +1,23 @@
 import React from "react"
 import ListGroup from "react-bootstrap/ListGroup"
 
+
 class RecipeList extends React.Component{
     render(){
         return(
             <ListGroup variant="flush">
-                <ListGroup.Item>Turkey and Cheese Sandwich</ListGroup.Item>
+                
+                <a href="#" class= "list-group-item">Turkey and Cheese Sandwich</a>
+                <a href="#" class= "list-group-item">Tomato Basil Soup</a>
+                <a href="#" class= "list-group-item">Ceasar Salad</a>
+                <a href="#" class= "list-group-item">Chicken Parmeasan</a>
+
+                {/* <ListGroup.Item>Turkey and Cheese Sandwich</ListGroup.Item>
                 <ListGroup.Item>Tomato Basil Soup</ListGroup.Item>
                 <ListGroup.Item>Ceasar Salad</ListGroup.Item>
-                <ListGroup.Item>Chicken Parmeasan</ListGroup.Item>
+                <ListGroup.Item>Chicken Parmeasan</ListGroup.Item> */}
             </ListGroup>
+
         )
     }
 }
