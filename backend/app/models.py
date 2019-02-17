4c841431f766b8d37c2f8530e8bc9475a2e72a89
Thomas Schroeder <schroe91@purdue.edu>
2019-02-17 18:49:17 -0500

    updating the models for the database

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -8,10 +9,24 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     token = db.Column(db.String(32), index=True, unique=True)
     created_recipes = db.relationship('Recipe', backref='creator')
+    user_ingredients = db.Column(db.String(140))
+    allergies = db.Column(db.String(140))
+    favorite_recipes = db.Column(db.String(140))
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    creator = db.Column(db.Integer)
+    creator = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    ingredient_list = db.Column(db.String(140))
+    prep_steps = db.Column(db.String(140))
+    prep_time = db.Column(db.Integer)
+    calories = db.Column(db.Integer)
+    carbs = db.Column(db.Integer)
+    fat = db.Column(db.Integer)
+    protein = db.Column(db.Integer)
+    is_vegan = db.Column(db.Boolean, default=False)
+    is_vegetarian = db.Column(db.Boolean, default=False)
+    is_glutenfree = db.Column(db.Boolean, default=False)
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
