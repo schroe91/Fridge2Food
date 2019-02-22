e9927efdab18a1607dc9c5799211fbe924c06d58
Matthew Retoff <matt.retoff@yahoo.com>
2019-02-22 00:40:18 -0500

    Editted checkbox logic

--- frontend/src/components/IngredientChecklist.js
+++ frontend/src/components/IngredientChecklist.js
@@ -4,7 +4,7 @@ import Checkbox from "./Checkbox";
 const OPTIONS = ["Bread", "Chicken", "Lettuce", "Beef", "Turkey", "Ham", "Rice", "Tomato", "Bacon", "Mayonnaise"];
 
 class IngredientChecklist extends Component {
-  constructor(props) {
+  /*constructor(props) {
     super(props);
     this.state = {
       checkboxes: OPTIONS.reduce(
@@ -13,11 +13,51 @@ class IngredientChecklist extends Component {
           [option]: false
         }),
       ),
-      numOfIngredients: 0
+      numOfIngredients: 0,
+      list: []
+    }
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }*/
+
+  componentWillMount = () => {
+    this.selectedCheckboxes = new Set();
+  }
+
+  toggleCheckbox = option => {
+    if (this.selectedCheckboxes.has(option)) {
+      alert("delete");
+      this.selectedCheckboxes.delete(option);
+    } else {
+      this.selectedCheckboxes.add(option);
+      alert("add");
     }
   }
 
-  handleCheckboxChange = changeEvent => {
+  handleFormSubmit = ev => {
+    ev.preventDefault();
+    var curr = 0;
+
+    for (const checkbox of this.selectedCheckboxes) {
+      alert(checkbox, 'is selected.');
+      curr++;
+    }
+    alert(curr);
+  }
+
+  createCheckbox = option => (
+    <Checkbox
+      label={option}
+      handleCheckboxChange={this.toggleCheckbox}
+      key={option}
+    />
+  );
+
+  createCheckboxes = () => (
+    OPTIONS.map(this.createCheckbox)
+  )
+
+  /*handleCheckboxChange = changeEvent => {
     const { name } = changeEvent.target;
 
     this.setState(prevState => ({
@@ -28,17 +68,20 @@ class IngredientChecklist extends Component {
     }));
   };
 
-  handleFormSubmit(list,number){
+  handleFormSubmit(ev) {
+    ev.preventDefault();
+    var curr = 0;
 
     Object.keys(this.state.checkboxes)
       .filter(checkbox => this.state.checkboxes[checkbox])
       .forEach(checkbox => {
-        if(list.indexOf(checkbox) === -1) {
-          list.unshift(checkbox);
-          this.props.number++; //Passes value to NumOfIngredients.js
-          this.props.view()
+        if(this.state.list.indexOf(checkbox) === -1) {
+          this.state.list.unshift(checkbox);
+          curr++;
         }
       });
+      this.props.funct(curr - this.state.numOfIngredients);
+      alert(this.state.checkboxes);
   };
 
   createCheckbox = option => (
@@ -48,19 +91,19 @@ class IngredientChecklist extends Component {
       onCheckboxChange={this.handleCheckboxChange}
       key={option}
     />
-  );
+  );*/
 
-  createCheckboxes = () => OPTIONS.map(this.createCheckbox);
+  //createCheckboxes = () => OPTIONS.map(this.createCheckbox);
 
   render() {
     return (
       <div className="container">
         <div className="row mt-5">
           <div className="col-sm-12">
-            <form onSubmit={this.handleFormSubmit} style={list}>
+            <form style={list} onSubmit={this.handleFormSubmit}>
               {this.createCheckboxes()}
               <div className="form-group mt-2">
-                <button type="submit" className="btn btn-primary" onClick={()=>{this.handleFormSubmit(this.props.listofIngredients, this.props.num)}}>
+                <button type="submit" className="btn btn-primary" onClick={this.handleFormSubmit}>
                   Save
                 </button>
               </div>
