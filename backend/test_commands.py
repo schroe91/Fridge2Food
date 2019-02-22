3444a284d8410747491de56497ef596c60e3aa7b
Zach Bridwell <bridwellzac@ymail.com>
2019-02-21 19:24:45 -0500

    Updated test commands

--- backend/test_commands.py
+++ backend/test_commands.py
@@ -6,22 +6,22 @@ app = create_app()
 app.app_context().push()
 
 #u = User(username='asdfaww', email='test@test.com')
-#r = Recipe(name='salted shit', creator_id=1, is_vegan=False)
+r = Recipe(name='salted pork', creator_id=1, is_vegan=False)
 
-#db.session.add(r)
+db.session.add(r)
 
 #db.session.add(Ingredient(name='shit'))
-"""
+
 db.session.add(Ingredient(name='salt'))
-db.session.add(Ingredient(name='shit'))
 db.session.add(Ingredient(name='butter'))
-db.session.add(Ingredient(name='black beans'))
+db.session.add(Ingredient(name='pork'))
 db.session.add(Ingredient(name='pinto beans'))
-"""
-#db.session.add(User(username='Anthony', email='test@example.com'))
-db.session.add(Recipe(name='Better Shit', creator_id=1))
+
+db.session.add(User(username='Anthony', email='test@example.com'))
+db.session.add(Recipe(name='Better Pork', creator_id=1))
 
 r = Recipe.query.get(2)
 r.ingredients.append(Ingredient.query.get(1))
 r.ingredients.append(Ingredient.query.get(2))
+r.ingredients.append(Ingredient.query.get(3))
 db.session.commit()
