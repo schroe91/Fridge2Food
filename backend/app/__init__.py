482048725b64de9e951d589e8e8ff7abeb05acdc
Anthony Kiselev <akiselev@purdue.edu>
2019-02-25 13:09:36 -0500

    Added backend support for cross origin resource sharing

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 
 from config import Config
 
@@ -13,7 +14,7 @@ login = LoginManager()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+    CORS(app)
     #login = LoginManager(app)
     db.init_app(app)
     migrate.init_app(app, db)
