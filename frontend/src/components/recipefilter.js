e03ffcc5083846509decacd0663cfabf7673ed79
Matthew Retoff <matt.retoff@yahoo.com>
2019-03-01 18:34:53 -0500

    Dropdown button styling

--- frontend/src/components/recipefilter.js
+++ frontend/src/components/recipefilter.js
@@ -7,18 +7,18 @@ class recipefilter extends Component{
     render(){
         return(
             <div id="filters">
-            <DropdownButton id="diet" title="Dietary Restrictions" style={button}>
+            <DropdownButton id="diet" title="Dietary Restrictions" size="sm">
                 <Dropdown.Item href="#/action-1">Vegetarian</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Vegan</Dropdown.Item>
                 <Dropdown.Item href="#/action-3">Pescatarian</Dropdown.Item>
                 <Dropdown.Item href="#/action-4">Gluten-free</Dropdown.Item>
                 <Dropdown.Item href="#/action-5">Lactose-free</Dropdown.Item>
             </DropdownButton>
-            <DropdownButton id="sort" title="Sort">
+            <DropdownButton id="sort" title="Sort" size="sm">
                 <Dropdown.Item href="#/action-1">Cooking Time</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Rating</Dropdown.Item>
             </DropdownButton>
-            <DropdownButton id="meal type" title="Meal Type">
+            <DropdownButton id="meal type" title="Meal Type" size="sm">
                 <Dropdown.Item href="#/action-1">Breakfast</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Lunch</Dropdown.Item>
                 <Dropdown.Item href="#/action-3">Dinner</Dropdown.Item>
@@ -29,7 +29,3 @@ class recipefilter extends Component{
     }
 }
 export default recipefilter;
-
-const button = {
-	display: "list",
-}
