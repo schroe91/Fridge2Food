dba7c932c2b44681b99027d28dcc2a7d6c0cbef6
adsank99 <sankara2@purdue.edu>
2019-03-03 16:38:39 -0500

    edited dropdown menu for sorting recipes

--- frontend/src/components/recipefilter.js
+++ frontend/src/components/recipefilter.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from "react-bootstrap/Dropdown";
 import './RecipeFilter.css';
+import DropdownMenu from "react-bootstrap/DropdownMenu";
 
 class recipefilter extends Component{
     constructor(props) {
@@ -28,10 +29,14 @@ class recipefilter extends Component{
                 <Dropdown.Item href="#/action-4">Gluten-free</Dropdown.Item>
                 <Dropdown.Item href="#/action-5">Lactose-free</Dropdown.Item>
             </DropdownButton>
-            <DropdownButton id="sort" title="Sort" size="sm">
+
+            
+                <DropdownButton id="sort" title="Sort By" size="sm">
                 <Dropdown.Item href="#/action-1">Cooking Time</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Rating</Dropdown.Item>
-            </DropdownButton>
+                <Dropdown.Item href="#/action-3">Calories</Dropdown.Item>
+                </DropdownButton>
+
             <DropdownButton id="meal type" title="Meal Type" size="sm">
                 <Dropdown.Item href="#/action-1">Breakfast</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Lunch</Dropdown.Item>
