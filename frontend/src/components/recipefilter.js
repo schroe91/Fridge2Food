f1d3206f0090b84e6c0c91d4f435a2a4896aa664
vivian-guo9 <vguo@purdue.edu>
2019-03-18 12:16:50 -0400

    install things for backend

--- frontend/src/components/recipefilter.js
+++ frontend/src/components/recipefilter.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from "react-bootstrap/Dropdown";
 import './RecipeFilter.css';
+import Checkbox from "./Checkbox";
 
 class recipefilter extends Component{
 
@@ -19,10 +20,94 @@ class recipefilter extends Component{
           isToggleOn: !state.isToggleOn
         }));
       }
+/*
+const OPTIONS = ["Bread", "Chicken", "Lettuce", "Beef", "Turkey", "Ham", "Rice", "Tomato", "Bacon", "Mayonnaise"];
+
+class IngredientChecklist extends Component { 
+  componentWillMount = () => {
+    this.selectedCheckboxes = new Set();
+  }
+
+  state = {
+    checkboxes: OPTIONS.reduce(
+      (options, option) => ({
+        ...options,
+        [option]: false
+      }),
+      {}
+    )
+  };
+
+  handleCheckboxChange = changeEvent => {
+    const { name } = changeEvent.target;
+    if(this.state.checkboxes[name])
+      this.props.funct(-1);
+    else
+      this.props.funct(1);
+
+    this.setState(prevState => ({
+      checkboxes: {
+        ...prevState.checkboxes,
+        [name]: !prevState.checkboxes[name]
+      }
+    }));
+  };
+  handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+
+    Object.keys(this.state.checkboxes)
+      .filter(checkbox => this.state.checkboxes[checkbox])
+      .forEach(checkbox => {
+        if(!this.selectedCheckboxes.has(checkbox.name) > -1) {
+          this.addItem(checkbox.name);
+        }
+      });
+  };
+
+  addItem(item) {
+    this.setState(({ selectedCheckboxes }) => ({
+      selectedCheckboxes: new Set(selectedCheckboxes).add(item)
+    }));
+  }
+
+  createCheckbox = OPTIONS => (
+    <Checkbox
+      label={OPTIONS}
+      isSelected={this.state.checkboxes[OPTIONS]}
+      onCheckboxChange={this.handleCheckboxChange}
+      key={OPTIONS}
+    />
+  );
+
+  createCheckboxes = () => OPTIONS.map(this.createCheckbox);
+
+  render() {
+    return (
+      <div style={checkList}>
+        <form onSubmit={this.handleFormSubmit}>
+          {this.createCheckboxes()}
+          <div className="form-group mt-2">
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default IngredientChecklist;
+const checkList = {
+  marginTop: "20px",
+  marginLeft: "15px",
+}
+*/
 
     render(){
         return(
             <div id="filters">
+
             <DropdownButton id="diet" title={this.title1} aria-expanded={this.state.isToggleOn} button onClick={this.handleClick}>
                 <Dropdown.Item href="#/action-1">Vegetarian</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Vegan</Dropdown.Item>
