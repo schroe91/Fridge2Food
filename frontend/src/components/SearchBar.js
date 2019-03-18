c9fb9bc0ef8bfd640c515c975dccf798608853e9
Zach Bridwell <bridwellzac@ymail.com>
2019-03-18 17:53:05 -0400

    changed searchBar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -12,7 +12,7 @@ class SearchBar extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this._handleKeyPress = this._handleKeyPress.bind(this);
       }
-    search(toSearch) {
+    /*search(toSearch) {
       console.log(toSearch);
       fetch('http://127.0.0.1:5000/api/recipe', {
           method: "POST",
@@ -21,7 +21,7 @@ class SearchBar extends React.Component {
           },
           body: JSON.stringify({recipe: toSearch})
       }).then( response => response.ok ).then(success => ( success ? this.setState({isAuth: success}) : this.setState({error: {message: "Incorrect email/password"}})))
-    }
+    }*/
       handleChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
