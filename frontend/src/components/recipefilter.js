7ed1685531a027c02b6c4ed7ac1b8a7c09668bfc
vivian-guo9 <vguo@purdue.edu>
2019-04-10 10:41:06 -0400

    fixed css

--- frontend/src/components/recipefilter.js
+++ frontend/src/components/recipefilter.js
@@ -14,11 +14,12 @@ class recipefilter extends Component{
             [option]: false
           }),
         )}
+        this.sortValue = 'Sort By';
         this.title1 = "Dietary Restrictions";
         this.title2 = "Sort By";
         this.title3 = "Meal Type";
         this.handleClick = this.handleClick.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        
       }
     filter_recipe() {
         fetch('http://127.0.0.1:5000/api/recipes', {
@@ -30,16 +31,15 @@ class recipefilter extends Component{
         })
 
       }
-    
+      handleChange(event) {
+        this.setState({sortValue: event.target.value});
+      }
       handleClick() {
         this.setState(state => ({
           isToggleOn: !state.isToggleOn
         }));
       }
 
-      handleChange() {
-        
-      }
 
     render(){
         return(
@@ -49,16 +49,21 @@ class recipefilter extends Component{
                 isSearchable={false} 
                 onChange={this.handleChange}
               />
-              <DropdownButton id="sort" title="Sort By"  >
-                <Dropdown.Item href="#/action-1">Cooking Time</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Rating</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Calories</Dropdown.Item>
+              <select value={this.state.sortValue} onChange={this.handleChange}>
+                <option value="Cooking Time">Cooking Time</option>
+                <option value="Rating">Rating</option>
+                <option value="Calories">Calories</option>
+              </select> 
+              <DropdownButton id="sort" title="Sort By" >
+                <Dropdown.Item as="button" eventKey='Cooking Time'>Cooking Time </Dropdown.Item>
+                <Dropdown.Item as="button" >Rating</Dropdown.Item>
+                <Dropdown.Item as="button" >Calories</Dropdown.Item>
                 </DropdownButton>
               <DropdownButton id="meal type" title="Meal Type">
-                  <Dropdown.Item href="#/action-1">Breakfast</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Lunch</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Dinner</Dropdown.Item>
-                  <Dropdown.Item href="#/action-4">Dessert</Dropdown.Item>
+                  <Dropdown.Item as="button" id="Breakfast">Breakfast</Dropdown.Item>
+                  <Dropdown.Item as="button" id="Lunch">Lunch</Dropdown.Item>
+                  <Dropdown.Item as="button" id="Dinner">Dinner</Dropdown.Item>
+                  <Dropdown.Item as="button" id="Dessert">Dessert</Dropdown.Item>
               </DropdownButton>
             </div>
         );
