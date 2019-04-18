fe95cc8353b688f7cce84d4254c2a199b84c6406
Matthew Retoff <matt.retoff@yahoo.com>
2019-04-17 23:32:08 -0400

    Fixed allergy display

--- frontend/src/components/IngredientDisplay.js
+++ frontend/src/components/IngredientDisplay.js
@@ -1,28 +1,28 @@
 import React from "react";
 
-class IngredientDisplay extends React.Component{
-  constructor(props){
+class IngredientDisplay extends React.Component {
+  constructor(props) {
     super(props);
     this.IngredientDisplay = this.IngredientDisplay.bind(this);
   }
-  
-  IngredientDisplay(){
-      return (<ul>
-        <h5>User Ingredients</h5>
-          {this.props.ingredients.map((item) => (
-          <li>
-            {item.name}
-          </li>
-        ))}
-      </ul>)
+
+  IngredientDisplay() {
+    return (<ul>
+      {this.props.ingredients.map((item) => (
+        <li>
+          {item.name}
+        </li>
+      ))}
+    </ul>)
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
+        <h5>User Ingredients</h5>
         {console.log("current ingredients: " + this.props.ingredient)}
-            <ul>{(this.props.ingredients.length > 0) ?  this.IngredientDisplay():<h5>No Ingredients</h5>}
-            </ul>
-        </div>
+        <ul>{(this.props.ingredients.length > 0) ? this.IngredientDisplay() : <h5>No Ingredients</h5>}
+        </ul>
+      </div>
     )
   }
-}export default IngredientDisplay
+} export default IngredientDisplay
