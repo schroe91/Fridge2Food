ff0be74b935179f63edaf4242b1e1dc07ace4e6c
Anthony Kiselev <akiselev@purdue.edu>
2019-04-19 12:21:35 -0400

    Fixed ingredients not being added in recipe

--- frontend/src/components/CreateRecipe.js
+++ frontend/src/components/CreateRecipe.js
@@ -82,7 +82,8 @@ class CreateRecipe extends Component {
 
   addnew(ingredient) {
     console.log("addnew =" + ingredient)
-    fetch('/api/ingredients', {
+      
+      fetch('/api/ingredients', {
       method: "POST",
       //mode: 'no-cors',
       headers: {
@@ -94,7 +95,9 @@ class CreateRecipe extends Component {
         return response.json();
       }
     }).then(data => {
-      this.setState({ ingredientsid: [...this.state.ingredientsid, data.id] }) //simple value
+	this.setState({ ingredientsid: [...this.state.ingredientsid, data.id] }) //simple value
+	console.log(this.state.ingredientsid);
+	console.log("Adding ingredients");
     })
   };
 
@@ -108,7 +111,7 @@ class CreateRecipe extends Component {
     const { ingredientsid } = this.state;
 
     console.log("ing id = " + ingredientsid);
-    this.createRecipe(recipe, ingredientsid, calories, carbs, date, prep_time, prep_steps);
+    this.createRecipe(recipe, this.state.ingredients, calories, carbs, date, prep_time, prep_steps);
 
   };
 
